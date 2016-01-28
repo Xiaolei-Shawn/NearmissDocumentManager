1e0f598d9ba202d4ee79f0702c6c69eb109f5159
joeyzhaozhao <joeyzhaozhao@gmail.com>
2016-01-28 16:30:09 +0200

    fix the input empty issue

--- app/home/index.controller.js
+++ app/home/index.controller.js
@@ -5,6 +5,18 @@
         .module('app')
         .controller('Home.IndexController', Controller);
 
+
+        var myApp = angular.module('myApp');
+// declare a function to run when the module bootstraps (during the 'config' phase)
+myApp.config(['NgAdminConfigurationProvider', function (nga) {
+    // create an admin application
+    var admin = nga.application('My First Admin');
+    // more configuration here later
+    // ...
+    // attach the admin application to the DOM and execute it
+    nga.configure(admin);
+}]);
+
     function Controller(UserService) {
         var vm = this;
 
