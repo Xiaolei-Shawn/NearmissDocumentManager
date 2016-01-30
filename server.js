16737421219d3cd393cafd261d8cab7126912036
xiaolei_shawn <xiaolei.shawn@gmail.com>
2016-01-30 22:27:03 +0200

    Revert "Add express router for rest api and git ignore file"
    This reverts commit 61600a2414d6aec3b575243e3e75dbf2be7c0390.

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ app.use('/register', require('./controllers/register.controller'));
 app.use('/app', require('./controllers/app.controller'));
 app.use('/api/users', require('./controllers/api/users.controller'));
 
-//route for rest api that is consumed by mobile
-app.use('/rest' require('./controller/rest.controller'));
-
 // make '/app' default route
 app.get('/', function (req, res) {
     return res.redirect('/app');
