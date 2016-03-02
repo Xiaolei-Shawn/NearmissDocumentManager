32cfacd38565bf72c02d4561fd6e80f56da74b5f
xiaolei_shawn <xiaolei.shawn@gmail.com>
2016-03-02 22:20:06 +0200

    Add report services: create, find one, and find all

--- services/report.service.js
+++ services/report.service.js
@@ -2,16 +2,43 @@ var dataService = require('services/data.service');
 
 var report = {
 	create : function(req, res){
-	var report = req.body;
+    	var report = req.body;
+        if(!report.reportid){
+            res.json({"STATUS": "404 NOT FOUND", "ERROR": "INVALID, NO reportid"});
+        } else {
+            dataService.createReport(report)
+              .then(function(report){
+                res.json({"STATUS": "200 OK"});
+            })
+            .catch(function(err){
+                res.json(err/*{"STATUS": "404 NOT FOUND", "ERROR": err}*/);
+            });
+        }
+        
+	},
 
-    dataService.createReport(report)
-      .then(function(report){
-        res.json({"STATUS": "200 OK"});
-    })
-    .catch(function(err){
-        res.json(err/*{"STATUS": "404 NOT FOUND", "ERROR": err}*/);
-    });
-	}
+    getOne : function(req, res){
+        var id = req.params.id;
+
+        dataService.getOneReport(id)
+          .then(function(report){
+            res.json(report);
+        })
+        .catch(function(err){
+            res.json(err/*{"STATUS": "404 NOT FOUND", "ERROR": err}*/);
+        });
+    },
+    getAll: function(req, res) {
+        dataService.getAllReports()
+         .then(function(allReports){
+            res.json(allReports);
+         })
+         .catch(function(err){
+            res.json();
+         })
+         .done();
+    
+    }
 };
 
 module.exports = report;
