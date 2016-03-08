317c51e2cd4c50ce8257c2bcb5cd6908526b7557
xiaolei_shawn <xiaolei.shawn@gmail.com>
2016-03-08 21:31:54 +0200

    Unit testing on request validation midware

--- test/unit.test.js
+++ test/unit.test.js
@@ -26,18 +26,57 @@ var assert = require('assert')
       });
   });
 
-  it('should not be able to consume the route /test since no token was sent', function(done) {
+  it('should not be able to consume the route /mapi/* since no token was sent', function(done) {
     request(app)
       .get('/mapi/template/nearmiss-template1')
       .expect(401, done);
   });
 
 
-  it('should be able to consume the route /test since token valid was sent', function(done) {
+  it('should be able to consume the route /mapi/* since token valid was sent', function(done) {
     request(app)
       .get('/mapi/template/nearmiss-template1')
       .set('x-access-token', token)
       .expect(200, done);
   });
+
+  it('should not be able to consume web api on mobile', function(done) {
+    request(app)
+      .get('/mapi/web/templates')
+      .set('x-access-token', token)
+      .expect(401)
+      .end(function (err, res) {
+        if(err) throw err;
+        res.body.message.should.be("Unaccessable path from mobile")
+      })
+      done()
+  });
+
+  it('should not be able to get token by nonexistent user', function(done) {
+    request(app)
+      .post('/mapi/mlogin')
+      .send({"x_key": "user@test.com",
+          "username": "user@test.com",
+          "password": "test"})
+      .expect(401)
+      .end(function (err, res) {
+        if(err) throw err;
+        res.body.message.should.be("Nonexistent User")
+      })
+      done()
+  });
+
+  it('should not return token when no key is sent', function(done) {
+    request(app)
+      .post('/mapi/mlogin')
+      .expect(401)
+      .end(function (err, res) {
+        if(err) throw err;
+        res.body.message.should.be("Invalid Token and Key")
+      })
+      done()
+  });
+
+
 });
 
