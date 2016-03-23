e522f094cceb63f94f00bd9d6e135453c30a1be5
joeyzhaozhao <joeyzhaozhao@gmail.com>
2016-03-23 09:55:02 +0200

    add new create template page

--- app/app-services/reportTemplate.service.js
+++ app/app-services/reportTemplate.service.js
@@ -9,18 +9,18 @@
         var service = {};
         //user use report template builder to build their own custom template
         service.CreateNewTemplate = CreateNewTemplate;
-     //   service.CreateNewReport = CreateNewReport;
+        service.CreateNewReport = CreateNewReport;
      //  service.Delete = Delete;
 
         return service;
 
         function CreateNewTemplate(template) {
-            return $http.post('/api/reports/web/template/', template).then(handleSuccess, handleError);
+            return $http.post('/api/resources/template/', template).then(handleSuccess, handleError);
         }
 
-/*        function CreateNewReport(report) {
-            return $http.put('/api/reports/' + report).then(handleSuccess, handleError);
-        }*/
+          function CreateNewReport(report) {
+            return $http.put('/api/resources/report/' + report).then(handleSuccess, handleError);
+        }
 
 
         // private functions
--- app/nearmissTemplate/nearmiss.controller.js
+++ app/nearmissTemplate/nearmiss.controller.js
@@ -21,161 +21,98 @@
         // the 'fields' attribute on the <formly-form> element
         vm.fields = [
             {
-                key: 'company',
-                type: 'input',
-                templateOptions: {
-                    type: 'text',
-                    label: 'Company',
-                    placeholder: 'Enter your Company'
+                "type": "checkbox",
+                "key": "company",
+                "templateOptions": {
+                    "label": "Company"
                 }
             },
             {
-                key: 'name',
-                type: 'input',
-                templateOptions: {
-                    type: 'text',
-                    label: 'Full name',
-                    placeholder: 'Enter your full name'
-
+                "type": "checkbox",
+                "key": "name",
+                "templateOptions": {
+                    "label": "Full Name"
                 }
             },
             {
-                key: 'department',
-                type: 'input',
-                templateOptions: {
-                    type: 'text',
-                    label: 'Department',
-                    placeholder: 'Enter your department'
+                "type": "checkbox",
+                "key": "department",
+                "templateOptions": {
+                    "label": "Department"
                 }
             },
             {
-                key: 'phone',
-                type: 'input',
-                templateOptions: {
-                    label: 'Phone number',
-                    placeholder: 'Enter your phone number'
+                "type": "checkbox",
+                "key": "phone",
+                "templateOptions": {
+                    "label": "Phone Number"
                 }
             },
             {
-                key: 'email',
-                type: 'input',
-                templateOptions: {
-                    type: 'email',
-                    label: 'Email',
-                    placeholder: 'Enter your Email'
-                }
-            },
-
-            {
-                key: 'roles',
-                type: 'radio',
-                templateOptions: {
-                    label: 'Roles',
-                    options: [{id: 1, title: "Manager"}, {id: 2, title: "Employee"}],
-                    valueProp: 'id',
-                    labelProp: 'title'
+                "type": "checkbox",
+                "key": "email",
+                "templateOptions": {
+                    "label": "Email"
                 }
             },
             {
-                template: '<hr />'
-            },
-
-            {
-                key: 'date',
-                type: 'datepicker',
-                templateOptions: {
-                    label: 'Date ',
-                    datepickerPopup: 'dd-MMMM-yyyy'
+                "type": "checkbox",
+                "key": "date",
+                "templateOptions": {
+                    "label": "Date"
                 }
             },
             {
-                key: 'time',
-                type: 'timepicker',
-                templateOptions: {
-                    label: 'Time'
+                "type": "checkbox",
+                "key": "time",
+                "templateOptions": {
+                    "label": "Time"
                 }
             },
             {
-                key: 'severityLevel',
-                type: 'select',
-                templateOptions: {
-                    label: 'Severity Level',
-                    // Call our province service to get a list
-                    // of provinces and territories
-                    options: [
-                        {name: 'Green', value: '1'},
-                        {name: 'Yellow', value: '2'},
-                        {name: 'Red', value: '3'}
-                    ]
-                }
-
-            },
-            {
-                key: 'cause',
-                type: 'select',
-                templateOptions: {
-                    label: 'Cause',
-                    // Call our province service to get a list
-                    // of provinces and territories
-                    options: [
-                        {name: ' Technological Failure', value: 'technological_failure'},
-                        {name: ' Working Environment ', value: 'working_environmentt'},
-                        {name: ' Lack of Protective Equipment', value: 'lack_of_protective_equipment'},
-                        {name: ' Disorder ', value: 'disorder '},
-                        {name: ' Machinery and Equipment', value: 'machinery_and_equipment'},
-                        {name: ' Unsafe Human Action ', value: 'unsafe_human_action '}
-                    ]
+                "type": "checkbox",
+                "key": "severityLevel",
+                "templateOptions": {
+                    "label": "Severity Level"
                 }
             },
             {
-                key: 'consequences',
-                type: 'select',
-                templateOptions: {
-                    label: 'Consequences',
-                    // Call our province service to get a list
-                    // of provinces and territories
-                    options: [
-                        {name: ' Personal Injury', value: 'personal_injury'},
-                        {name: ' Machine Breakdown', value: 'machine_breakdown'},
-                        {name: ' Fire ', value: 'fire'},
-                        {name: ' Environment ', value: 'environment '},
-                        {name: ' Damage', value: 'damage'},
-                        {name: ' Other', value: 'other'}
-                    ]
+                "type": "checkbox",
+                "key": "cause",
+                "templateOptions": {
+                    "label": "Cause"
                 }
-
-            },
-
-            {
-                key: 'location',
-                type: 'select',
-                templateOptions: {
-                    label: 'Location',
-                    options: [
-                        {}
-                    ]
-                }
-
             },
             {
-                key: 'details',
-                type: 'textarea',
-                templateOptions: {
-                    "placeholder": "Please write down other details...",
-                    "label": "Other details",
-                    "rows": 4,
-                    "cols": 15
+                "type": "checkbox",
+                "key": "consequences",
+                "templateOptions": {
+                    "label": "Consequences"
+                }
+            },
+            {
+                "type": "checkbox",
+                "key": "location",
+                "templateOptions": {
+                    "label": "Location"
+                }
+            },
+            {
+                "type": "checkbox",
+                "key": "others",
+                "templateOptions": {
+                    "label": "Other Details"
                 }
             }
-        ];
 
+        ];
         vm.template = null;
         vm.createTemplate = createTemplate;
 
         function createTemplate() {
             ReportTemplateService.CreateNewTemplate(vm.fields)
                 .then(function () {
-                    FlashService.Success('Template Submitted');
+                    FlashService.Success('The Nearmiss report template has been successfully submitted !');
                 })
                 .catch(function (error) {
                     FlashService.Error(error);
