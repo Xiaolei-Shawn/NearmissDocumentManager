1772a9b09f49435a326667f2477f77dc4e636607
xiaolei_shawn <xiaolei.shawn@gmail.com>
2016-04-01 22:37:26 +0300

    Refine the response of validatUser request

--- services/auth.service.js
+++ services/auth.service.js
@@ -3,6 +3,7 @@ var config = require('config.json');
 var dataService = require('services/data.service');
 var S = require('string');
 var _ = require('lodash');
+var Q = require('q');
 
 var auth = {
  
@@ -20,32 +21,59 @@ var auth = {
 
     if(S(username).contains('@')){
       //email used in username
+      
       dataService.validateUser(username, null, password)
         .then(function(user){
-            dataService.getAllTemplates()
+          dataService.getAllTemplates()
             .then(function(allTemplates){
-              res.status(200);
-              res.json( {
-                token: genToken(user),
-                user: user,
-                //templates: _.map(allTemplates, _.property('templateid'))
-                templateinfo: _.map(allTemplates, function(template){
-                  return {
-                    templatename: template.title,
-                    templatetype: template.type,
-                    templateid: template._id
-                  }
-                })
-              });
-              return;
+                dataService.getAllReports()
+                  .then(function(allReports){
+                    res.status(200);
+                     res.json({
+                      token: genToken(user),
+                      user: user,
+                      //templates: _.map(allTemplates, _.property('templateid'))
+                      templates: _.map(allTemplates, function(template){
+                                    return {
+                                      title: template.title,
+                                      type: template.type,
+                                      _id: template._id
+                                    }
+                                  }),
+                      reports: _.map(allReports, function(report){
+                                    return {
+                                      title: report.title,
+                                      type: report.type,
+                                      _id: report._id
+                                    }
+                                  }),
+                    });
+                  })
+                  .catch(function(err){
+                    res.status(200);
+                     res.json({
+                      token: genToken(user),
+                      user: user,
+                      //templates: _.map(allTemplates, _.property('templateid'))
+                      templates: _.map(allTemplates, function(template){
+                                    return {
+                                      title: template.title,
+                                      type: template.type,
+                                      _id: template._id
+                                    }
+                                  }),
+                      reports: err
+                    });
+                  })
             })
             .catch(function(err){
-              res.status(401);
-              res.json( {
-                "status": 401,
-                "message": "Not a valid user",
-                "error": err
-              });
+                res.status(200);
+                     res.json({
+                      token: genToken(user),
+                      user: user,
+                      //templates: _.map(allTemplates, _.property('templateid'))
+                      templates: err
+                });
             })
             //res.json(genToken(user));
         })
@@ -71,9 +99,9 @@ var auth = {
                 //templates: _.map(allTemplates, _.property('templateid'))
                 templateinfo: _.map(allTemplates, function(template){
                   return {
-                    templatename: template.title,
-                    templatetype: template.type,
-                    templateid: template._id
+                    title: template.title,
+                    type: template.type,
+                    _id: template._id
                   }
                 })
               });
@@ -121,6 +149,13 @@ function genToken(user) {
   var token = jwt.sign({user: user}, config.secret, {expiresIn : 60 * 60 * 2});
   return token;
 
+}
+
+function queryTemplatesAndReports(){
+  return Q.all([
+              dataService.getAllTemplates(),
+              dataService.getAllReports()
+            ]);
 }
  
 module.exports = auth;
